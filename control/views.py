8d275905cc20927011b577e77ae84ff5635f49cf
magicbullet <magicbulletsoft@gmail.com>
2024-08-04 00:06:03 +0530

    bugfix

--- control/views.py
+++ control/views.py
@@ -24,13 +24,11 @@ def toggle_accessory(request, accessory_id):
     accessory = get_object_or_404(Accessory, id=accessory_id)
     accessory.status = not accessory.status
     accessory.save()
-    accessory.pin_number
     GPIO.setup(accessory.pin_number, GPIO.OUT)
     if accessory.status:
         GPIO.output(accessory.pin_number, GPIO.HIGH)
     else:
         GPIO.output(accessory.pin_number, GPIO.LOW)
-
     return redirect('accessory_list')
 
 
